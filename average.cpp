ee2847a74723c88694e5f8ab1a477050a3eb417b
Oatcomm <cholakorn.oat@gmail.com>
2026-02-27 15:14:37 +0700

    finish

--- average.cpp
+++ average.cpp
@@ -1,1 +1,15 @@
-
+#include <iostream>
+using namespace std;
+int main(int argc, char* argv[]) {
+    float total = 0;
+    if(argc <2){
+    cout <<"Please input numbers to find average.";
+    }else{
+    for (int i =1;i<argc;i++){
+         total += atof(argv[i]);
+    }
+    cout <<"---------------------------------\n";
+    cout << "Average of " << argc-1 << " numbers = " << total/(argc-1) << "\n";
+    cout <<"---------------------------------\n";
+}
+}
